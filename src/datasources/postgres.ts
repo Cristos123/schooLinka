bdf6e99d1952c1756f1ca138c4d25693d493840e
cristos123 <osunkile4live@gmail.com>
2023-09-05 11:50:23 +0100

    deployment

--- src/datasources/postgres.ts
+++ src/datasources/postgres.ts
@@ -29,39 +29,48 @@
 import { env } from "../config/index";
 import { Sequelize } from "sequelize";
 
-let postgresClient;
+// let postgresClient;
 
-if (env.NODE_ENV === "production") {
-  // Production configuration  URL
-  postgresClient = new Sequelize(env.URL, {
-    logging: false,
-    pool: {
-      max: 2,
-      min: 0,
-      acquire: 3000,
-      idle: 0,
-    },
-  });
-} else {
-  // Development configuration with localhost
-  postgresClient = new Sequelize(
-    env.DATABASE as string,
-    env.USER as string,
-    env.PASSWORD as string,
-    {
-      host: "localhost",
-      dialect: "postgres",
-      port: parseInt(env.PORT as string),
-      logging: false,
-      pool: {
-        max: 2,
-        min: 0,
-        acquire: 3000,
-        idle: 0,
-      },
-    }
-  );
-}
+// if (env.NODE_ENV === "production") {
+//   // Production configuration  URL
+//   postgresClient = new Sequelize(env.URL, {
+//     logging: false,
+//     pool: {
+//       max: 2,
+//       min: 0,
+//       acquire: 3000,
+//       idle: 0,
+//     },
+//   });
+// } else {
+//   // Development configuration with localhost
+//   postgresClient = new Sequelize(
+//     env.DATABASE as string,
+//     env.USER as string,
+//     env.PASSWORD as string,
+//     {
+//       host: "localhost",
+//       dialect: "postgres",
+//       port: parseInt(env.PORT as string),
+//       logging: false,
+//       pool: {
+//         max: 2,
+//         min: 0,
+//         acquire: 3000,
+//         idle: 0,
+//       },
+//     }
+//   );
+// }
+const postgresClient = new Sequelize(env.URL, {
+  logging: false,
+  pool: {
+    max: 2,
+    min: 0,
+    acquire: 3000,
+    idle: 0,
+  },
+});
 
 postgresClient
   .sync({ alter: true })
