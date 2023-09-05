73c8ca4976db96564f173bbb00adf3fd7b2a20e8
cristos123 <osunkile4live@gmail.com>
2023-09-05 11:34:38 +0100

    chaning production url for database

--- src/datasources/postgres.ts
+++ src/datasources/postgres.ts
@@ -1,39 +1,14 @@
-import { env } from "../config/index";
-import { Sequelize } from "sequelize";
-
-const postgresClient = new Sequelize(
-  env.DATABASE as string,
-  env.USERNAME as string,
-  env.PASSWORD as string,
-  {
-    host: env.HOSTNAME || "localhost",
-    dialect: "postgres",
-    port: parseInt(env.PORT as string),
-    logging: false,
-    pool: {
-      max: 2,
-      min: 0,
-      acquire: 3000,
-      idle: 0,
-    },
-  }
-);
-
-postgresClient
-  .sync({ alter: true })
-  .then(() => console.log("Postgres connected successfully!"))
-  .catch((err) => console.log(err));
-
-export default postgresClient;
-
 // import { env } from "../config/index";
 // import { Sequelize } from "sequelize";
 
-// let postgresClient;
-
-// if (env.NODE_ENV === "production") {
-//   // Production configuration  URL
-//   postgresClient = new Sequelize(env.HOST, {
+// const postgresClient = new Sequelize(
+//   env.DATABASE as string,
+//   env.USERNAME as string,
+//   env.PASSWORD as string,
+//   {
+//     host: env.HOSTNAME || "localhost",
+//     dialect: "postgres",
+//     port: parseInt(env.PORT as string),
 //     logging: false,
 //     pool: {
 //       max: 2,
@@ -41,27 +16,8 @@ export default postgresClient;
 //       acquire: 3000,
 //       idle: 0,
 //     },
-//   });
-// } else {
-//   // Development configuration with localhost
-//   postgresClient = new Sequelize(
-//     env.DATABASE as string,
-//     env.USER as string,
-//     env.PASSWORD as string,
-//     {
-//       host: "localhost",
-//       dialect: "postgres",
-//       port: parseInt(env.PORT as string),
-//       logging: false,
-//       pool: {
-//         max: 2,
-//         min: 0,
-//         acquire: 3000,
-//         idle: 0,
-//       },
-//     }
-//   );
-// }
+//   }
+// );
 
 // postgresClient
 //   .sync({ alter: true })
@@ -69,3 +25,47 @@ export default postgresClient;
 //   .catch((err) => console.log(err));
 
 // export default postgresClient;
+
+import { env } from "../config/index";
+import { Sequelize } from "sequelize";
+
+let postgresClient;
+
+if (env.NODE_ENV === "production") {
+  // Production configuration  URL
+  postgresClient = new Sequelize(env.URL, {
+    logging: false,
+    pool: {
+      max: 2,
+      min: 0,
+      acquire: 3000,
+      idle: 0,
+    },
+  });
+} else {
+  // Development configuration with localhost
+  postgresClient = new Sequelize(
+    env.DATABASE as string,
+    env.USER as string,
+    env.PASSWORD as string,
+    {
+      host: "localhost",
+      dialect: "postgres",
+      port: parseInt(env.PORT as string),
+      logging: false,
+      pool: {
+        max: 2,
+        min: 0,
+        acquire: 3000,
+        idle: 0,
+      },
+    }
+  );
+}
+
+postgresClient
+  .sync({ alter: true })
+  .then(() => console.log("Postgres connected successfully!"))
+  .catch((err) => console.log(err));
+
+export default postgresClient;
